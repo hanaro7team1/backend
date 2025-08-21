1b325bf69eacb98109dc475ef09426d1f37ba6a5
Kunbam7 <kunh9905@gmail.com>
2025-08-21 14:16:54 +0900

    reservation entity

--- src/main/java/reservation/entity/Reservation.java
+++ src/main/java/reservation/entity/Reservation.java
@@ -1,5 +1,7 @@
 package reservation.entity;
 
+import java.time.LocalDate;
+
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
@@ -7,6 +9,8 @@ import common.entity.BaseEntity;
 import house.entity.House;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -24,10 +28,19 @@ public class Reservation extends BaseEntity {
 	private Long id;
 
 	@Column(nullable = false)
+	private LocalDate startDate;
+
+	@Column(nullable = false)
+	private LocalDate endDate;
+
+	@Column(nullable = false)
 	private short personCnt;
 
 	@Column(nullable = false)
-	private Boolean isFarm = true;
+	private Boolean isFarm;
+
+	@Enumerated(EnumType.STRING)
+	private ResrvStatus resrvStatus;
 
 	@ManyToOne
 	@JoinColumn(name = "member",
