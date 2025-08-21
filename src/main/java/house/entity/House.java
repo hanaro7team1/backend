2f39c24142850320c88e52a5a0e4b54555c52921
Kunbam7 <kunh9905@gmail.com>
2025-08-21 13:49:46 +0900

    house entity

--- src/main/java/house/entity/House.java
+++ src/main/java/house/entity/House.java
@@ -3,15 +3,22 @@ package house.entity;
 import common.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import member.entity.HostMember;
 
 @Entity
 public class House extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(length = 16, nullable = false)
+	private String title;
 
 	@Column(length = 64, nullable = false)
 	private String address;
@@ -22,5 +29,25 @@ public class House extends BaseEntity {
 	@Column(nullable = false)
 	private Short areaSize = 0;
 
+	@Column(length = 512, nullable = true)
+	private String description;
 
+	@Column(nullable = false)
+	private Boolean isHomestay;
+
+	@Column(length = 31, nullable = true)
+	private String ownerPhone;
+
+	@ManyToOne
+	@JoinColumn(name = "host",
+		foreignKey = @ForeignKey(
+			name = "fk_House_HostMember",
+			foreignKeyDefinition = """
+					foreign key (host)
+					   references HostMember(id)
+					    on DELETE cascade on UPDATE cascade
+				"""
+		)
+	)
+	private HostMember host;
 }
