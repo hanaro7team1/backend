11f01e90259f935e0c0325460de2ecb41cfeecb9
lavendmin <lavendmin639@gmail.com>
2025-08-25 10:31:46 +0900

    Test: 사랑방, 사랑방 예약 가능 날짜 add Test

--- src/main/java/com/sido/backend/house/entity/HouseAvailableDate.java
+++ src/main/java/com/sido/backend/house/entity/HouseAvailableDate.java
@@ -10,22 +10,25 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class HouseAvailableDate {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(nullable = false)
-	private LocalDate startDate;
-
-	@Column(nullable = false)
-	private LocalDate endDate;
+	private LocalDate date;
 
 	@ManyToOne
 	@JoinColumn(name = "house",
---
+++ src/main/java/com/sido/backend/house/repository/HouseAvailableDateRepository.java
@@ -0,0 +1,8 @@
+package com.sido.backend.house.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sido.backend.house.entity.HouseAvailableDate;
+
+public interface HouseAvailableDateRepository extends JpaRepository<HouseAvailableDate, Long> {
+}
