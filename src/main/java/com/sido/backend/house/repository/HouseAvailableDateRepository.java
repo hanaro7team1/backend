7eaddb9eb0137be1a236a4fe844720bfc3d3ea7c
lavendmin <lavendmin639@gmail.com>
2025-08-26 16:19:33 +0900

    Remove: House 관련 파일 삭제

--- src/main/java/com/sido/backend/house/repository/HouseAvailableDateRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sido.backend.house.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sido.backend.house.entity.HouseAvailableDate;
-
-public interface HouseAvailableDateRepository extends JpaRepository<HouseAvailableDate, Long> {
-}
