e070b756b90f71a789d92f2429a70ab240e03d79
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-09-02 14:15:58 +0900

    Refactor: PasswordUpdateRequestDTO 속성에 annotation 추가

--- src/main/java/com/sido/backend/member/dto/PasswordUpdateRequestDTO.java
+++ src/main/java/com/sido/backend/member/dto/PasswordUpdateRequestDTO.java
@@ -1,11 +1,22 @@
 package com.sido.backend.member.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class PasswordUpdateRequestDTO {
+	@NotBlank(message = "기존 비밀번호를 입력해 주세요")
 	private String currentPassword;
+
+	@NotBlank(message = "변경할 비밀번호를 입력해 주세요")
+	@Size(min = 8, max = 20, message = "영문자,숫자,특수문자를 포함한 8~20자")
+	@Pattern(
+		regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d).*$",
+		message = "변경할 비밀번호는 영문자,숫자,특수문자를 포함한 8~20자여야 합니다"
+	)
 	private String newPassword;
 }
