5415daca1a8bf08f0203fb6c1361a8b580eafbbc
lavendmin <lavendmin639@gmail.com>
2025-09-02 14:40:59 +0900

    Refactor: DTO import문 정리

--- src/main/java/com/sido/backend/reservation/dto/ReservationCreateResponseDTO.java
+++ src/main/java/com/sido/backend/reservation/dto/ReservationCreateResponseDTO.java
@@ -2,17 +2,19 @@ package com.sido.backend.reservation.dto;
 
 import com.fasterxml.jackson.annotation.JsonIncludeProperties;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.sido.backend.reservation.dto.ReservationCommonDTOs.ReservationInfoDTO;
+import com.sido.backend.reservation.dto.ReservationCommonDTOs.StaySummaryDTO;
 import com.sido.backend.reservation.entity.ResrvStatus;
 
 public record ReservationCreateResponseDTO(
 	Long reservationId,
 
 	@JsonUnwrapped
-	ReservationCommonDTOs.StaySummaryDTO staySummary, // stayId, title, address (상세 주소 X)
+	StaySummaryDTO staySummary, // stayId, title, address (상세 주소 X)
 
 	@JsonUnwrapped
 	@JsonIncludeProperties({"startDate", "endDate", "personCnt"})
-	ReservationCommonDTOs.ReservationInfoDTO reservationInfo,
+	ReservationInfoDTO reservationInfo,
 
 	ResrvStatus resrvStatus
 ) {
