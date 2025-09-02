1408e5c1d1f3c898c1dc43a4ba67ec31a753a077
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-09-02 14:32:50 +0900

    Feat: 전화번호 양식 pattern validation 추가

--- src/main/java/com/sido/backend/member/dto/PhoneUpdateRequestDTO.java
+++ src/main/java/com/sido/backend/member/dto/PhoneUpdateRequestDTO.java
@@ -1,6 +1,7 @@
 package com.sido.backend.member.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,5 +9,6 @@ import lombok.Setter;
 @Setter
 public class PhoneUpdateRequestDTO {
 	@NotBlank
+	@Pattern(regexp = "^0\\d{1,2}-\\d{3,4}-\\d{4}$", message = "전화번호 양식이 올바르지 않습니다.")
 	private String phone;
 }
