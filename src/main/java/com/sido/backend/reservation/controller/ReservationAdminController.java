d74a2696cf025ede57789fc3220fdf2d65115a39
lavendmin <lavendmin639@gmail.com>
2025-09-02 19:01:45 +0900

    Rename: 시골관리자 예약 현황 DTO, 메소드 이름 변경 & ReservationListDTO -> ReservationListItemDTO 이름 변경

--- src/main/java/com/sido/backend/reservation/controller/ReservationAdminController.java
+++ src/main/java/com/sido/backend/reservation/controller/ReservationAdminController.java
@@ -1,12 +1,13 @@
 package com.sido.backend.reservation.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.sido.backend.reservation.dto.ReservationSummaryDTO;
+import com.sido.backend.reservation.dto.ReservationOverviewDTO;
 import com.sido.backend.reservation.service.ReservationService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -21,10 +22,11 @@ public class ReservationAdminController {
 	private final ReservationService reservationService;
 
 	@Operation(summary = "예약 현황 조회", description = "방문 전, 방문 중, 방문 완료 수")
-	@GetMapping("/summary")
-	public ResponseEntity<ReservationSummaryDTO> getReservationSummary(
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@GetMapping("/overview")
+	public ResponseEntity<ReservationOverviewDTO> getReservationOverview(
 		@AuthenticationPrincipal(expression = "memberId") Long memberId) {
-		ReservationSummaryDTO reservationSummary = reservationService.getReservationSummary(memberId);
+		ReservationOverviewDTO reservationSummary = reservationService.getReservationOverview(memberId);
 		return ResponseEntity.ok(reservationSummary);
 	}
 
--- src/main/java/com/sido/backend/reservation/service/ReservationService.java
+++ src/main/java/com/sido/backend/reservation/service/ReservationService.java
@@ -5,7 +5,7 @@ import com.sido.backend.reservation.dto.ReservationConfirmResponseDTO;
 import com.sido.backend.reservation.dto.ReservationCreateRequestDTO;
 import com.sido.backend.reservation.dto.ReservationCreateResponseDTO;
 import com.sido.backend.reservation.dto.ReservationDetailResponseDTO;
-import com.sido.backend.reservation.dto.ReservationSummaryDTO;
+import com.sido.backend.reservation.dto.ReservationOverviewDTO;
 
 public interface ReservationService {
 	ReservationCreateResponseDTO createReservation(Long memberId, Long stayId,
@@ -15,8 +15,8 @@ public interface ReservationService {
 		ReservationConfirmRequestDTO confirmRequest);
 
 	ReservationDetailResponseDTO getReservationDetail(Long memberId, Long reservationId);
-  
+
 	void cancelReservation(Long memberId, Long reservationId);
 
-	ReservationSummaryDTO getReservationSummary(Long memberId);
+	ReservationOverviewDTO getReservationOverview(Long memberId);
 }
