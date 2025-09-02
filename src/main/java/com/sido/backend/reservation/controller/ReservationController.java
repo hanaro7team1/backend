7e399acf1f02efb04026e26aef1186940e5e6678
lavendmin <lavendmin639@gmail.com>
2025-09-02 09:36:01 +0900

    feat: 예약 상세 조회

--- src/main/java/com/sido/backend/reservation/controller/ReservationController.java
+++ src/main/java/com/sido/backend/reservation/controller/ReservationController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import com.sido.backend.reservation.dto.ReservationConfirmRequestDTO;
 import com.sido.backend.reservation.dto.ReservationConfirmResponseDTO;
 import com.sido.backend.reservation.dto.ReservationCreateRequestDTO;
 import com.sido.backend.reservation.dto.ReservationCreateResponseDTO;
+import com.sido.backend.reservation.dto.ReservationDetailResponseDTO;
 import com.sido.backend.reservation.service.ReservationService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -46,5 +48,13 @@ public class ReservationController {
 		ReservationConfirmResponseDTO confirmResponse = reservationService.confirmReservation(memberId,
 			reservationId, confirmRequest);
 		return ResponseEntity.ok().body(confirmResponse);
+	}
+
+	@Operation(summary = "예약 상세 조회")
+	@GetMapping("/api/reservations/{reservationId}")
+	public ResponseEntity<ReservationDetailResponseDTO> getReservationDetail(
+		@AuthenticationPrincipal(expression = "memberId") Long memberId, @PathVariable Long reservationId) {
+		ReservationDetailResponseDTO detailResponse = reservationService.getReservationDetail(memberId, reservationId);
+		return ResponseEntity.ok().body(detailResponse);
 	}
 }
--- src/main/java/com/sido/backend/reservation/dto/ReservationDetailResponseDTO.java
+++ src/main/java/com/sido/backend/reservation/dto/ReservationDetailResponseDTO.java
@@ -1,22 +1,25 @@
 package com.sido.backend.reservation.dto;
 
-import com.fasterxml.jackson.annotation.JsonIncludeProperties;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.sido.backend.reservation.entity.ResrvStatus;
 
 public record ReservationDetailResponseDTO(
 	Long reservationId,
 
+	ResrvStatus resrvStatus,
+
 	String memberName,
 
 	String memberPhone,
+
+	Boolean isHomestay,
 
 	String ownerName,
 
 	String ownerPhone,
 
 	@JsonUnwrapped
-	@JsonIncludeProperties({"title", "address"})
-	ReservationCommonDTOs.StaySummaryDTO staySummary, // address: 상세주소 포함
+	ReservationCommonDTOs.StaySummaryDTO staySummary, // stayId, title, address(상세주소 포함)
 
 	@JsonUnwrapped
 	ReservationCommonDTOs.ReservationInfoDTO reservationInfo // startDate, endDate, personCnt, isFarm
